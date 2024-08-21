c4bc91df1c71634f87e6cf468848ef9a4168fc26
Hardik <101357543+Apocalypse-0729@users.noreply.github.com>
2024-08-21 12:34:00 +0530

    Create SUBSET_count_subset_sum.cpp

---
+++ dp/SUBSET_count_subset_sum.cpp
@@ -0,0 +1,23 @@
+public:
+	const int mod=1e9+7;
+	int func(int i, int sum, int arr[], vector<vector<int>> &dp){
+        if(i==0){
+            if(sum==0 && arr[0]==0)return 2;
+            if(sum==0 || sum==arr[0]) return 1;
+            return 0;
+        }
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        int notake=func(i-1,sum,arr,dp);
+        int take=0;
+        if(arr[i]<=sum){
+            take=func(i-1,sum-arr[i],arr,dp);
+        }
+        return dp[i][sum]=(take+notake)%(mod);
+    }
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        // int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return func(n-1,sum,arr,dp);
+	}

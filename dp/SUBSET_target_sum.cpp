5a036d656c95293b0a89c5ff17cb92f0700788d1
Hardik <101357543+Apocalypse-0729@users.noreply.github.com>
2024-08-21 12:36:01 +0530

    Create SUBSET_target_sum.cpp

---
+++ dp/SUBSET_target_sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    int func(int i, int sum, vector<int> &arr, vector<vector<int>> &dp){
+        if(i==0){
+            if(sum==0 && arr[0]==0)return 2;
+            if(sum==0 || sum==arr[0]) return 1;
+            return 0;
+        }
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        int notake=func(i-1,sum,arr,dp);
+        int take=0;
+        if(arr[i]<=sum){
+            take=func(i-1,sum-arr[i],arr,dp);
+        }
+        return dp[i][sum]=(take+notake)%(mod);
+    }
+    int findTargetSumWays(vector<int>& arr, int d) {
+        int totSum=0;
+        int n=arr.size();
+        for(auto it:arr) totSum+=it;
+        if(totSum-d<0) return 0;
+        if((totSum-d)%2==1) return 0;
+        int sum=(totSum-d)/2;
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return func(n-1,sum,arr,dp);
+    }
+};
